347637c91612dca443431014f96eb85de864c0fb
Harviator <78506767+harviator@users.noreply.github.com>
2021-06-18 19:17:33 -0400

    So I can create a note, but it's not going to the db.json file.  I'm not sure if it's rendering.  I've been working on these api routes, but not making very much progress.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uuidv1 = require('uuidv1');
 
 const app = express();
 
@@ -13,16 +14,50 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Routes
-// Serves the html pages
+// HTML Route
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-//serves the api data
-app.get('/api/notes', (req, res) => res.json(notes));
-//adds the user data
-app.post('/api/notes',(req, res) => {
-    notes.push(req.body);
-    res.json(notes);
+
+// API Routes 
+app.get('/api/notes', (req, res) => res.json(notes)); 
+app.get('.api/notes/:note', (req, res ) => {
+
+    console.log(req.params.id);
+    //const currentNote = notes.filter()
+})
+//app.get to get 1 note //use a filter to filter ids
+// Note data
+app.post('/api/notes', (req, res) => { // Add new note
+    
+    const id = uuidv1();
+    
+    const newNote = {
+        id: id,
+        title: req.body.title,
+        text: req.body.text,
+    };
+    
+    notes.push(newNote);
+
+    console.log(newNote);
+
+    console.log("New Note created");
+});
+app.put('api/notes', (req, res) => { // Update notes
+    // what the frick do I write here?
+});
+app.delete('api/notes', (req, res) => { // Delete a note
+    //what the frick do I write here?
 })
 
+//Default HTML Route
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
 // Starts server
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
+/*
+
+Notes:
+    -The notes need unique ids
+
+*/
